31fbbb8ee7fd2d8831645f3d106076768039d89e
Breno S F <BrenoSilvaFortunato@gmail.com>
2021-03-23 09:06:39 -0300

    responsive, slides, animations

---
+++ js/script.js
@@ -0,0 +1,20 @@
+if(window.SimpleSlide){
+
+    new SimpleSlide({
+        slide: 'quote', // nome do atributo data-slide="principal"
+        //nav: true, // se deve ou não mostrar a navegação
+        auto: true, // se o slide deve passar automaticamente
+        time: 5000, // tempo de transição dos slides
+        pauseOnHover: true, // pausa a transição automática
+      });
+    new SimpleSlide({
+        slide: 'portifolio', // nome do atributo data-slide="principal"
+        nav: true, // se deve ou não mostrar a navegação
+        auto: true, // se o slide deve passar automaticamente
+        time: 5000, // tempo de transição dos slides
+        pauseOnHover: true, // pausa a transição automática
+      });
+}
+if(window.SimpleAnime){
+  new SimpleAnime();
+}
